e85516e1b522de017f0c18d2293e27896d9da358
UmbraDracona <shadowfire0117@gmail.com>
2020-02-10 15:04:45 -0600

    added search results page, fixed entries to include author names

--- CrunchTimeD2D/TestBookStore/urls.py
+++ CrunchTimeD2D/TestBookStore/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 from . import views
+from .views import SearchResultsView
 
 urlpatterns = [
     path('', views.index, name='index'),
+    #path('search', views.search, name='search'),
+    path('search/', SearchResultsView.as_view(), name='search'),
     path('book_detail/', views.view_book_detail, name='view_book_detail'),
     path('submit_onix/', views.submit_onix, name='submit_onix'),
 ]
