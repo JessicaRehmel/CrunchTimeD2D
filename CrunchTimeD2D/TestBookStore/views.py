43eb4ceaed45c59a71c09d538366c4ab24377753
David Noble <danoble.proj@gmail.com>
2020-02-10 15:50:55 -0600

    Onix validated and saved to file

--- CrunchTimeD2D/TestBookStore/views.py
+++ CrunchTimeD2D/TestBookStore/views.py
@@ -2,15 +2,13 @@ from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect
 from django.urls import reverse
 from django.views import generic
-from rest_framework import generics, permissions, renderers
+from rest_framework import generics, permissions, renderers, status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework.reverse import reverse
-<<<<<<< HEAD
-import onixcheck
-=======
+
+import onixcheck, os
 from .models import *
->>>>>>> master
 
 from .models import OnixFile
 from .serializers import OnixSerializer
@@ -29,14 +27,19 @@ def view_book_detail(request):
 
 @api_view(['POST'])
 def submit_onix(request):
-    print(request)
-    #f = open("validateOnix.xml", "w")
-    #f.write("" + request.data)
-    #f.close()
-    #errors = onixcheck("validateOnix.xml")
-    #if (errors[0].short[0:5] != "ERROR"):
-    #f = open("onix.xml", "w")
-    #f.write(request.data)
-    #f.close()
-    #return Response("", status=status.HTTP_201_CREATED)
-    #return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    f = open("tempOnix.xml", "w")
+    f.write(request.POST['data'])
+    f.close()
+    try:
+        errors = onixcheck.validate("onix.xml")
+        if (len(errors) == 0):
+            f = open("onix.xml", "w")
+            f.write(request.POST['data'])
+            f.close()
+            os.remove("tempOnix.xml")
+            return Response("", status=status.HTTP_201_CREATED)
+        os.remove("tempOnix.xml")
+        return Response("Invalid ONIX", status=status.HTTP_400_BAD_REQUEST)
+    except:
+        os.remove("tempOnix.xml")
+        return Response("Invalid XML", status=status.HTTP_400_BAD_REQUEST)
